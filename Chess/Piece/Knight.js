4a6748fe9dac8b083c9ba85a60b56dd3a2bc48cf
Amitay Horwitz <amitay@babylon.com>
2011-03-02 13:41:40 +0200

    knight movement

--- Chess/Piece/Knight.js
+++ Chess/Piece/Knight.js
@@ -4,6 +4,34 @@ Chess.Piece.Knight = new Class({
     
     getSymbol: function() {
         return this.player.isWhite() ? '♘' : '♞';
+    },
+
+    getMoves: function() {
+        var moves = [], offset = [1, 2, -1, -2], offsetX, offsetY, square;
+
+        for (var i = 0; i < 4; i++) {
+            offsetX = offset[i];
+            for (var _i = 0; _i < 4; _i++) {
+                offsetY = offset[_i];
+                if (offsetX.abs() == offsetY.abs()) {
+                    continue;
+                }
+                square = this.square.getSibling(offsetX, offsetY);
+                if (square) {
+                    if (square.piece) {
+                        // Square is occupied
+                        if (square.piece.player != this.player) {
+                            // Occupied by other player - piece can be captured
+                            moves.push(square);
+                        }
+                    } else {
+                        moves.push(square);
+                    }
+                }
+            }
+        }
+
+        return moves;
     }
 
 });
