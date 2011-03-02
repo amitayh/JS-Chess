bd98efba609acd38a0d4f33fe72fb744b99ebb12
Amitay Horwitz <amitay@babylon.com>
2011-03-02 12:11:57 +0200

    rook / bishop movement

--- Chess/Piece/Bishop.js
+++ Chess/Piece/Bishop.js
@@ -4,6 +4,32 @@ Chess.Piece.Bishop = new Class({
     
     getSymbol: function() {
         return this.player.isWhite() ? '♗' : '♝';
+    },
+
+    getMoves: function() {
+        var moves = [], offset = [1, -1], offsetX, offsetY, square;
+
+        for (var i = 0; i < 2; i++) {
+            offsetX = offset[i];
+            for (var _i = 0; _i < 2; _i++) {
+                offsetY = offset[_i];
+                square = this.square;
+                // Add all squares in path
+                while (square = square.getSibling(offsetX, offsetY)) {
+                    if (square.piece) {
+                        // Square is occupied
+                        if (square.piece.player != this.player) {
+                            // Occupied by other player - piece can be captured
+                            moves.push(square);
+                        }
+                        break;
+                    }
+                    moves.push(square);
+                }
+            }
+        }
+
+        return moves;
     }
 
 });
--- Chess/Piece/Rook.js
+++ Chess/Piece/Rook.js
@@ -4,6 +4,29 @@ Chess.Piece.Rook = new Class({
     
     getSymbol: function() {
         return this.player.isWhite() ? '♖' : '♜';
+    },
+
+    getMoves: function() {
+        var moves = [], offsets = [[1, 0], [-1, 0], [0, 1], [0, -1]], offset, square;
+        
+        for (var i = 0; i < 4; i++) {
+            offset = offsets[i];
+            square = this.square;
+            // Add all squares in path
+            while (square = square.getSibling(offset[0], offset[1])) {
+                if (square.piece) {
+                    // Square is occupied
+                    if (square.piece.player != this.player) {
+                        // Occupied by other player - piece can be captured
+                        moves.push(square);
+                    }
+                    break;
+                }
+                moves.push(square);
+            }
+        }
+
+        return moves;
     }
 
 });
